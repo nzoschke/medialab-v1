680b6627e0ecc29a0b69df1447b537d50dca945a
Noah Zoschke <noah@mixable.net>
2024-05-26 10:13:12 -0700

    Timeline (#6)
    - Timeline via GSAP
    - Two-way binding to UI

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+  import { gsap } from "gsap";
+  import { Timeline, type State, type Status } from "@components/timeline";
+
+  let structure = $state({
+    phrase: "low",
+  });
+
+  // bind timeline and UI state
+  const timeline = Timeline();
+
+  let ui: State = $state({
+    progress: 0,
+    status: "new" as Status,
+  });
+
+  timeline.opts = {
+    onStatus: (s) => {
+      ui.status = s;
+    },
+    onProgress: (p) => {
+      ui.progress = p;
+    },
+  };
+
+  $effect(() => {
+    timeline.progress = ui.progress;
+  });
+
+  for (var i = 0; i <= 5; i++) {
+    timeline.timeline.set(
+      structure,
+      {
+        phrase: `phrase-${i}`,
+        data: {
+          duration: 1,
+        },
+      },
+      i,
+    );
+
+    timeline.timeline.call(
+      (p, i) => {
+        console.log("call", p, i);
+      },
+      ["param", i],
+      i,
+    );
+  }
+</script>
+
+<div>{structure.phrase}</div>
+<div>{ui.status}</div>
+
+<button
+  class="btn"
+  onclick={() => {
+    timeline.pauseResume();
+  }}
+>
+  {ui.status == "play" ? "Pause" : "Play"}
+</button>
+
+<input type="range" min="0" max="1" step="0.001" bind:value={ui.progress} class="range rounded-none" />
+
+<div class="relative flex h-6 w-full rounded-box">
+  {#each timeline.divs() as tw}
+    <div class="absolute h-6 border" style="left: {tw.left}%; width: {tw.width}%;"></div>
+  {/each}
+</div>
+
+<style>
+  /* https://github.com/saadeghi/daisyui/blob/master/src/components/styled/range.css */
+  ::-webkit-slider-thumb {
+    border-radius: 0;
+    width: 5px;
+    --filler-offset: 0rem;
+    box-shadow:
+      0 0 0 1px var(--range-shdw) inset,
+      var(--focus-shadow, 0 0),
+      calc(var(--filler-size) * -1 - var(--filler-offset)) 0 0 var(--filler-size);
+  }
+</style>
---
+++ src/components/timeline.ts
@@ -0,0 +1,112 @@
+import { gsap } from "gsap";
+
+export interface State {
+  progress: number;
+  status: Status;
+}
+
+export type Status = "new" | "play" | "pause" | "complete";
+
+interface Opts {
+  onStatus?: (s: Status) => void;
+  onProgress?: (p: number) => void;
+}
+
+export const Timeline = (opts?: Opts) => {
+  const timeline = gsap.timeline({
+    defaults: {
+      ease: "none",
+      immediateRender: false,
+    },
+    paused: true,
+  });
+
+  const state: State = new Proxy(
+    {
+      progress: 0,
+      status: "new" as Status,
+    },
+    {
+      set(obj, prop, value) {
+        if (prop == "progress") {
+          obj["progress"] = value;
+          opts?.onProgress && opts.onProgress(value);
+        }
+
+        if (prop == "status") {
+          obj["status"] = value;
+          opts?.onStatus && opts.onStatus(value);
+        }
+
+        return true;
+      },
+    },
+  );
+
+  timeline.vars.onComplete = () => {
+    state.status = "complete";
+  };
+
+  timeline.vars.onUpdate = () => {
+    state.progress = timeline.progress();
+  };
+
+  return {
+    divs: () => {
+      const tws = timeline.getChildren(false, true, false).filter((tw) => tw.data) as gsap.core.Tween[];
+      return tws.map((tw, i) => {
+        const start = tw.startTime();
+        const next = tws.at(i + 1);
+        const end = next ? next.startTime() : tw.startTime();
+        const duration = end - start;
+
+        return {
+          data: tw.data,
+          duration,
+          end,
+          start,
+          left: (start / timeline.duration()) * 100,
+          width: (duration / timeline.duration()) * 100,
+        };
+      });
+    },
+    get duration() {
+      return timeline.duration();
+    },
+    pauseResume: () => {
+      if (state.status == "new") {
+        state.status = "play";
+        timeline.play();
+        return;
+      }
+
+      if (state.status == "play") {
+        state.status = "pause";
+        timeline.pause();
+        return;
+      }
+
+      if (state.status == "pause") {
+        state.status = "play";
+        timeline.play();
+        return;
+      }
+
+      if (state.status == "complete") {
+        state.progress = 0;
+        timeline.progress(0);
+        state.status = "play";
+        timeline.play();
+        return;
+      }
+    },
+    set opts(o: Opts) {
+      opts = o;
+    },
+    set progress(p: number) {
+      state.progress = p;
+      timeline.progress(p);
+    },
+    timeline,
+  };
+};
